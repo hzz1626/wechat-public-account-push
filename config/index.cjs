715399f5e5e7733af154754687cc4d6b507b8608
hzz1626 <111802911+hzz1626@users.noreply.github.com>
2023-09-06 20:35:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ USE_PASSAGE: 'push-deer'
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU25302TTG2kSK18amYjlXxtCFIDoJEKc01azLPj',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
